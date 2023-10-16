cd7e833d76197704b9ce43504d4704032ac85489
英吾 <mono.eigo@gmail.com>
2023-10-17 08:05:21 +0900

    items下準備

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,13 @@
 <?php
 
+use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\InertiaTestController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('/items', ItemController::class)->middleware(['auth', 'verified']);
 
 Route::get('/inertia-test', function () {
     return Inertia::render('InertiaTest');
@@ -25,11 +17,11 @@ Route::get('/component-test', function () {
     return Inertia::render('ComponentTest');
 });
 
-Route::get('/inertia/index', [InertiaTestController::class, 'index']) -> name('inertia.index');
-Route::get('/inertia/create', [InertiaTestController::class, 'create']) -> name('inertia.create');
-Route::post('/inertia', [InertiaTestController::class, 'store']) -> name('inertia.store');
-Route::get('/inertia/show/{id}', [InertiaTestController::class, 'show']) -> name('inertia.show');
-Route::delete('/inertia/{id}', [InertiaTestController::class, 'delete']) -> name('inertia.delete');
+Route::get('/inertia/index', [InertiaTestController::class, 'index'])->name('inertia.index');
+Route::get('/inertia/create', [InertiaTestController::class, 'create'])->name('inertia.create');
+Route::post('/inertia', [InertiaTestController::class, 'store'])->name('inertia.store');
+Route::get('/inertia/show/{id}', [InertiaTestController::class, 'show'])->name('inertia.show');
+Route::delete('/inertia/{id}', [InertiaTestController::class, 'delete'])->name('inertia.delete');
 
 
 Route::get('/', function () {
