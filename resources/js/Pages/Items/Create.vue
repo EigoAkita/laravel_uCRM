04726a48d37351de364074bc2e6875f09f8fcfb2
英吾 <mono.eigo@gmail.com>
2023-10-18 00:41:25 +0900

    update : 商品登録画面内のボタンのテキスト変更

--- resources/js/Pages/Items/Create.vue
+++ resources/js/Pages/Items/Create.vue
@@ -86,7 +86,7 @@ const storeItem = () => {
                         <button
                           class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
                         >
-                          Button
+                          商品登録
                         </button>
                       </div>
                     </div>
