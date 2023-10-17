92965c12512eab856037d4af3776bfec3e5ca8e1
英吾 <mono.eigo@gmail.com>
2023-10-18 01:12:33 +0900

    add : Inertiaのバージョン変更に伴い、手動でValidationErrorsコンポーネントを追加（超重要） add : ValidationErrorsコンポーネントをお試し追加

---
+++ resources/js/Components/ValidationErrors.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  errors: Object,
+});
+
+// computed : 値の変更を監視して、値の変更等があればcomputed内の処理実行
+// 以下の場合は、definePropsで渡ってきたerrorsが一件以上あれば、hasErrorsをtrueに変更
+const hasErrors = computed(() => Object.keys(props.errors).length > 0);
+
+</script>
+
+<template>
+  <div v-if="hasErrors">
+    <div class="font-medium text-red-600">問題が発生しました。</div>
+
+    <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+      <li v-for="(error, key) in props.errors" :key="key">{{ error }}</li>
+    </ul>
+  </div>
+</template>
--- resources/js/Pages/Inertia/Create.vue
+++ resources/js/Pages/Inertia/Create.vue
@@ -1,32 +1,32 @@
 <script setup>
-
-import { reactive } from 'vue'
-import { Inertia } from '@inertiajs/inertia'
+import { reactive } from "vue";
+import { Inertia } from "@inertiajs/inertia";
+import BreezeValidationErrors from "@/Components/ValidationErrors.vue";
 
 defineProps({
-    errors: Object
-})
+  errors: Object,
+});
 
 // 以下の様にする事で、form.titleやform.contentなどで中身の参照が可能
 const form = reactive({
-    title: null,
-    content: null,
-})
+  title: null,
+  content: null,
+});
 
 const submitFunction = () => {
-    //　以下の様にする事で、formオブジェクトのデータを渡す事が可能
-    Inertia.post('/inertia', form)
-}
-
+  //　以下の様にする事で、formオブジェクトのデータを渡す事が可能
+  Inertia.post("/inertia", form);
+};
 </script>
 
 <template>
-    <!-- ページ読込防止の為（パフォーマンス低下の為）@submitにpreventを付与 -->
-    <form @submit.prevent="submitFunction">
-        <input type="text" name="title" v-model="form.title"><br>
-        <div v-if="errors.title">{{ errors.title }}</div>
-        <input type="text" name="content" v-model="form.content">
-        <div v-if="errors.content">{{ errors.content }}</div>
-        <button>送信</button>
-    </form>
+  <BreezeValidationErrors :errors="errors" />
+  <!-- ページ読込防止の為（パフォーマンス低下の為）@submitにpreventを付与 -->
+  <form @submit.prevent="submitFunction">
+    <input type="text" name="title" v-model="form.title" /><br />
+    <div v-if="errors.title">{{ errors.title }}</div>
+    <input type="text" name="content" v-model="form.content" />
+    <div v-if="errors.content">{{ errors.content }}</div>
+    <button>送信</button>
+  </form>
 </template>
